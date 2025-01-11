fa3edc8a3dc76abea522603e21fdcade0e7fb44a
Giovanni Basso <119098857+Basso-Giovanni@users.noreply.github.com>
2025-01-11 08:29:06 +0100

    Update index.php di cinema

--- Cinema/index.php
+++ Cinema/index.php
@@ -1,9 +1,9 @@
 <?php
     include('config/db.php');
-    setcookie("user_email", "", time() - 3600, "/", "", true, true);
-    setcookie("user_id", "", time() - 3600, "/", "", true, true);
-    setcookie("user_nome", "", time() - 3600, "/", "", true, true);
-    setcookie("preferenze_generi", "", time() - 3600, "/", "", true, true);
+    setcookie("user_email", "", time() - 3600, "/", "", true, true); 
+    setcookie("user_id", "", time() - 3600, "/", "", true, true); 
+    setcookie("user_nome", "", time() - 3600, "/", "", true, true); 
+    setcookie("preferenze_generi", "", time() - 3600, "/", "", true, true); 
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST') 
     {
@@ -19,9 +19,9 @@
             
             if (password_verify($password, $user['pass'])) 
             {
-                setcookie("user_email", $user['email'], time() + (86400 * 30), "/", "", true, true); // Email
-                setcookie("user_id", $user['IDUtente'], time() + (86400 * 30), "/", "", true, true); // ID
-                setcookie("user_nome", $user['nominativo'], time() + (86400 * 30), "/", "", true, true); // Nome
+                setcookie("user_email", $user['email'], time() + (86400 * 30), "/", "", true, true); // Email, 24 h
+                setcookie("user_id", $user['IDUtente'], time() + (86400 * 30), "/", "", true, true); // ID, 24 h
+                setcookie("user_nome", $user['nominativo'], time() + (86400 * 30), "/", "", true, true); // Nome, 24 h
                 header("Location: http://localhost/2024/Cinema/pages/preferenze.php");
                 exit;
             } 
