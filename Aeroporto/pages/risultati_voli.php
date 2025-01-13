cd7b3db9c1c7d830e8e499e53439c848a740ea13
Basso-Giovanni <9698115@itisrossi.vi.it>
2025-01-13 18:48:27 +0100

    Correzione aeroporto

--- Aeroporto/pages/risultati_voli.php
+++ Aeroporto/pages/risultati_voli.php
@@ -1,36 +1,38 @@
-<?php require 'config.php'; ?>
+<?php require '../config/db.php'; ?>
 <!DOCTYPE html>
 <html lang="it">
-<head>
-    <meta charset="UTF-8">
-    <meta name="author" content="Giovanni Basso">
-    <title>Risultati Voli</title>
-</head>
-<body>
-    <h1>Risultati della Ricerca</h1>
-    <ul>
-        <?php
-        $origine = $_GET['aeroporto_part'];
-        $destinazione = $_GET['aeroporto_arr'];
-        $data = $_GET['DataViaggio'];
-        
-        echo $origine . " ". $destinazione;
+    <head>
+        <meta charset="UTF-8">
+        <meta name="author" content="Giovanni Basso">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Centro di controllo</title>
+    </head>
+    <body>
+        <h1>Risultati della Ricerca</h1>
+        <ul>
+            <?php
+                $origine = $_GET['aeroporto_part'];
+                $destinazione = $_GET['aeroporto_arr'];
+                $data = $_GET['DataViaggio'];
+                
+                echo $origine . " ". $destinazione;
 
-        $query = "SELECT * FROM voli WHERE aeroporto_part = '" . $origine . "' AND aeroporto_arr = '" . $destinazione . "' AND DATE(data_partenza) = '" . $data . "';";        
-        $result = $conn->query($query);
+                $query = "SELECT * FROM voli WHERE aeroporto_part = '" . $origine . "' AND aeroporto_arr = '" . $destinazione . "' AND DATE(data_partenza) = '" . $data . "';";        
+                $result = $conn->query($query);
 
-        if ($result->num_rows > 0)
-        {
-            while ($row = $result->fetch_assoc())
-            {
-                echo "<li>" . $row["id"] . " - Aeroporto di partenza: " . $row["aeroporto_part"] . " - Aeroporto di arrivo: " . $row["aeroporto_arr"] . " - Aereo: " . $row["aereo"] . " - Partenza: " . $row["data_partenza"] . " Arrivo: " . $row["data_arrivo"] . "</li>";
-            }
-        }
-        else
-        {
-            echo "Nessun risultato!";
-        }
-        ?>
-    </ul>
-</body>
+                if ($result->num_rows > 0)
+                {
+                    while ($row = $result->fetch_assoc())
+                    {
+                        echo "<li>" . $row["id"] . " - Aeroporto di partenza: " . $row["aeroporto_part"] . " - Aeroporto di arrivo: " . $row["aeroporto_arr"] . " - Aereo: " . $row["aereo"] . " - Partenza: " . $row["data_partenza"] . " Arrivo: " . $row["data_arrivo"] . "</li>";
+                    }
+                }
+                else
+                {
+                    echo "Nessun risultato!";
+                }
+            ?>
+        </ul>
+    </body>
+    <?php $conn->close(); ?>
 </html>
