963772061fd8b604feca8d3adfdcd85b664052d5
Basso-Giovanni <9698115@itisrossi.vi.it>
2025-02-21 18:27:15 +0100

    modifica struttura sim 2017

--- Sim_2017/config/db.php
+++
@@ -1,6 +0,0 @@
-<?php
-    define('DB_HOST', 'localhost');
-    define('DB_NAME', 'carpooling');
-    define('DB_USER', 'root');
-    define('DB_PASS', '');
-?>
--- Sim_2017/public/nav.php
+++
@@ -1,6 +0,0 @@
-<ul>
-    <li><a href="home.php">Home</a></li>
-    <li><a href="search.php">Cerca</a></li>
-    <li><a href="pers.php">Area personale</a></li>
-    <li><a href="logout.php">Esci</a></li>
-</ul>
